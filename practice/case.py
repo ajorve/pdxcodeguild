867394f36f7eaaec74bd346e07fb16149ec23cb5
ajorve <anders@codeinfusion.com>
2017-01-06 10:01:57 -0800

    Created new .gitignore file and changes to practice labs

---
+++ practice/case.py
@@ -0,0 +1,24 @@
+"""
+
+>>> snake_to_camel('this_is_snake_case')
+'ThisIsSnakeCase'
+>>> which_case('this_test_text')
+'snake_case.'
+>>> which_case('this_is_snake_case')
+'snake_case.'
+>>> which_case('ThisIsCamelCase')
+'CamelCase.'
+
+"""
+
+# 3 Functions of looping
+
+def snake_to_camel(word):
+    word = word.replace('_', ' ').title().replace(' ', '')
+    return(word)
+
+def which_case(word):
+    if '_' in word:
+        return "snake_case."
+    else:
+        return "CamelCase."
---
+++ practice/i_before_e.py
@@ -0,0 +1,21 @@
+"""
+Check that a word follows "I before E except after C".
+
+>>> check("recieve")
+recieve doesn't follow the rule
+
+>>> check("receive")
+receive does follow the rule
+"""
+
+def check_word(word):
+    if 'c' in word:
+        pass
+    standard_rule = 'ei'
+    exception_to_rule = 'ie'
+
+    word check for standard_rule
+    else return exception_to_rule
+
+    if 'ie' in x =
+    elif 'ei' in x =
