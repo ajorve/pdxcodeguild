7cc0df0bc12a99af974a7f55d6c50d3d6a3ca98b
ajorve <anders@codeinfusion.com>
2017-02-28 16:32:43 -0800

    created new trimet app

---
+++ JS Practice/bus stop finder/site.js
@@ -0,0 +1,60 @@
+(function app() {
+    "use strict";
+
+    var markers = [];
+
+    (function get_loc() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var user_loc = {lat: position.coords.latitude, lng: position.coords.longitude};
+            searchRadius(user_loc);
+        });
+    })();
+
+    function searchRadius(user_loc) {
+        var $submit = $('#submit');
+
+        $submit.on('click', function (event) {
+            event.preventDefault();
+            var $meters = $('#input_meters').val();
+            get_data(user_loc, $meters);
+        })
+    }
+
+    function add_stop(stops) {
+        var marker;
+        $.each(stops, function (index, stop) {
+            var stop_location = {lat: stop.lat, lng: stop.lng};
+            var image = 'trimet.png';
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(stop_location.lat, stop_location.lng),
+                animation: google.maps.Animation.DROP,
+                icon: image,
+                title: 'Bus Stop',
+                map: map
+            });
+            markers.push(marker);
+        })
+    }
+
+    function get_data(user_loc, meters) {
+        $.ajax({
+            url: 'https://developer.trimet.org/ws/V1/stops?data',   // Target Server
+            method: 'GET',                                            // Request Verb
+            data: {
+                appID: '000272B7746A2C92D2B026E39',
+                ll: user_loc.lat + ", " + user_loc.lng,
+                json: 'true',
+                meters: meters
+            },                                                       // Request Params
+            success: function (rsp) {                                // Success Handler
+                var bus_stops = rsp.resultSet.location;
+                add_stop(bus_stops);
+            },
+            error: function (err) {                                  // Error Handler
+                console.log(err);
+            }
+        });
+    }
+
+    // Captures User Geolocation
+})();
